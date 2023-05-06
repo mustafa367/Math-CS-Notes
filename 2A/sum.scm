4ac700728cca5585a607f4da3b423b44355ce305
mustafa367 <57695339+mustafa367@users.noreply.github.com>
2023-05-05 23:35:48 -0400

    2A bug fix

--- 2A/sum.scm
+++ 2A/sum.scm
@@ -8,10 +8,9 @@
 	    next
 	    b))))
 
-(define (id x) (x))
-(define (incr x) (+ 1 x))
-(disp (sum id 1 incr 4))
+(define (id x) x)
+(disp (sum id 1 1+ 4))
 
-;(define (square x) (* x x))
-;(disp (sum square 1 (lambda (x) (+ 1 x)) 4))
+(define (square x) (* x x))
+(disp (sum square 1 (lambda (x) (+ 1 x)) 4))
 
